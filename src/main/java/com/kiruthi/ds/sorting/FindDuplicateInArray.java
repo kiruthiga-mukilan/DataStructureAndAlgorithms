b8f72b3d1ca4e0fca4425aa31ea4543ac19ccc56
P Kiruthiga <kip@europe.corp.altran.com>
2018-11-02 11:13:54 +0530

    Sorting Advanced

---
+++ src/main/java/com/kiruthi/ds/sorting/FindDuplicateInArray.java
@@ -0,0 +1,31 @@
+package com.kiruthi.ds.sorting;
+
+class FindDuplicateInArray
+{
+    void printRepeating(int arr[], int size)
+    {
+        int i;
+        System.out.println("The repeating elements are : ");
+
+        for (i = 0; i < size; i++)
+        {
+            if (arr[Math.abs(arr[i])] >= 0)
+                arr[Math.abs(arr[i])] = -arr[Math.abs(arr[i])];
+            else
+                System.out.print(Math.abs(arr[i]) + " ");
+        }
+    }
+
+    // Driver program
+    public static void main(String[] args)
+    {
+        FindDuplicateInArray duplicate = new FindDuplicateInArray();
+        int arr[] = {1, 2, 3, 1, 3, 6, 6};
+        int arr_size = arr.length;
+
+        duplicate.printRepeating(arr, arr_size);
+    }
+}
+
+// This code has been contributed by Mayank Jaiswal
+
